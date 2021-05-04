20dbb1987c3a7f0bbef9dbad596af55161c9c642
Niashi <mathias.pn7@gmail.com>
2021-05-04 18:00:07 -0300

    Adicionando microGameiros

---
+++ microGameiros/src/main/java/com/generation/microGameiros/MicroGameirosApplication.java
@@ -0,0 +1,13 @@
+package com.generation.microGameiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGameirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroGameirosApplication.class, args);
+	}
+
+}
---
+++ microGameiros/src/test/java/com/generation/microGameiros/MicroGameirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.microGameiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGameirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
